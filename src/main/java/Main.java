c3a5dd445b7f9c4a838be9989ffd78bed778de7b
Eric Canull <ericcanull1000@msn.com>
2017-06-24 00:36:54 -0500

    converted to fxml controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,28 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Dimension2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	private static final Dimension2D DIM = new Dimension2D(1220, 680);
-        
-	public static void main(String[] args) {
-		launch(args);
-	}
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public Parent createContent() {
-		final WebBrowser browser = new WebBrowser(DIM);
-		return browser;
-	}
+public class Main extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setResizable(true);
-		Scene scene = new Scene(createContent());
-		primaryStage.setTitle("Eric's Web Demo");
-	//scene.setUserAgentStylesheet(getClass().getResource("style/template.css").toExternalForm());
-		scene.getStylesheets().add("style/template.css");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	public void start(Stage stage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLWebPanel.fxml"));
+
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	/* @param args the command line arguments */
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
--- src/main/java/WebController.java
+++ src/main/java/WebController.java
@@ -1,168 +1,133 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Scanner;
-
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
 import org.w3c.dom.Attr;
 import org.w3c.dom.NodeList;
 
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener.Change;
-import javafx.event.ActionEvent;
-import javafx.geometry.Dimension2D;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebHistory;
-import javafx.scene.web.WebHistory.Entry;
-import javafx.scene.web.WebView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class WebBrowser extends BorderPane {
-      
-    private final WebView webView;
-    private final WebEngine webEngine;
-    private final ComboBox<String> addressBox;
-    private final TextField searchField;
-    private final Button backButton;
-    private final Button forwardButton;
-    private final Button proceedButton;
-    private final Button searchButton;
-    private final ProgressBar progressBar;
-    private final Label statusLabel;
-    
-    public WebBrowser(final Dimension2D dimension) {
-        
-        this.setMinSize(dimension.getWidth(), dimension.getHeight());
-        this.setPrefSize(dimension.getWidth(), dimension.getHeight());
-        
-        backButton = new Button("\uD83E\uDC78");
+/**
+ * Created by andje22 on 6/23/17.
+ */
+public class WebController extends BorderPane implements Initializable {
+
+    @FXML private WebView webView;
+    @FXML private WebEngine webEngine;
+    @FXML private ComboBox<String> addressBox;
+    @FXML private TextField searchField;
+    @FXML private Button backButton;
+    @FXML private Button forwardButton;
+    @FXML private Button proceedButton;
+    @FXML private Button searchButton;
+    @FXML private ProgressBar progressBar;
+    @FXML private Label statusLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        initializeBrowser();
+    }
+
+    private void initializeBrowser() {
+        backButton.setText("\uD83E\uDC78");
         backButton.setOnAction(this::backButtonListener);
-        
-        forwardButton = new Button("\u2794");
+
+        forwardButton.setText("\u2794");
         forwardButton.setDefaultButton(true);
         forwardButton.setOnAction(this::forwardButtonListener);
-        
-        proceedButton = new Button("Go");
+
+        proceedButton.setText("Go");
         proceedButton.setOnAction(this::proceedButtonListener);
-        
-        final HBox buttonGroup = new HBox();
-        buttonGroup.setSpacing(5);
-        buttonGroup.setPadding(new Insets(10, 5, 10, 5));
-        buttonGroup.getChildren().addAll(backButton, forwardButton, proceedButton);
-        
-        addressBox = new ComboBox<>();
+
         addressBox.setItems(FXCollections.observableArrayList());
         addressBox.setValue("http://www.google.com");
         addressBox.setOnAction(this::proceedButtonListener);
         addressBox.setEditable(true);
-        addressBox.setMaxWidth(Double.MAX_VALUE);
-        
-        searchField = new TextField();
-        searchField.setPromptText("\uD83D\uDD0D Search");
-        
-        searchButton = new Button("\uD83D\uDD0D");
+
+        searchField.setPromptText("🔍" + "Search");
+
+        searchButton.setText("🔍");
         searchButton.setDefaultButton(true);
         searchButton.setOnAction(this::searchButtonListener);
-        
-        statusLabel = new Label("Status: ");
-        progressBar = new ProgressBar(0);
-        
-        webView = new WebView();
+
+
         webEngine = webView.getEngine();
-     //   webEngine.setUserStyleSheetLocation(getClass().getResource("/style/style.css").toString());
+        //   webEngine.setUserStyleSheetLocation(getClass().getResource("/style/style.css").toString());
         webEngine.load(addressBox.getValue());
         progressBar.progressProperty().bind(webEngine.getLoadWorker().progressProperty());
         webEngine.getLoadWorker().stateProperty().addListener(this::stateChangeListener);
-      //  webEngine.locationProperty().addListener(this::urlChangeListener);
-        
+
         final WebHistory history = webEngine.getHistory();
         history.getEntries().addListener(this::historyListener);
-        
-        final GridPane root = new GridPane();
-        root.getStyleClass().add("pane");
-        GridPane.setConstraints(buttonGroup,  0, 0, 1, 1, HPos.LEFT,   VPos.CENTER, Priority.NEVER,  Priority.NEVER);
-        GridPane.setConstraints(addressBox,   1, 0, 1, 1, HPos.CENTER, VPos.CENTER, Priority.ALWAYS, Priority.NEVER);
-        GridPane.setConstraints(searchField,  2, 0, 1, 1, HPos.RIGHT,  VPos.CENTER, Priority.NEVER,  Priority.NEVER);
-        GridPane.setConstraints(searchButton, 3, 0, 1, 1, HPos.RIGHT,  VPos.CENTER, Priority.NEVER,  Priority.NEVER);
-        GridPane.setConstraints(webView,      0, 1, 4, 1, HPos.LEFT,   VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
-        GridPane.setConstraints(statusLabel,  0, 2, 1, 1, HPos.LEFT,   VPos.CENTER, Priority.NEVER,  Priority.NEVER);
-        GridPane.setConstraints(progressBar,  3, 2, 3, 1, HPos.RIGHT,  VPos.CENTER, Priority.NEVER,  Priority.NEVER);
-        
-        GridPane.setMargin(addressBox,   new Insets(5, 0, 5, 0));
-        GridPane.setMargin(searchField,  new Insets(5, 5, 5, 5));
-        GridPane.setMargin(searchButton, new Insets(5, 8, 5, 0));
-        GridPane.setMargin(statusLabel,  new Insets(5, 0, 5, 5));
-        GridPane.setMargin(progressBar,  new Insets(5, 5, 5, 5));
-        
-        root.addRow(0, buttonGroup, addressBox, searchField, searchButton);
-        root.addRow(1, webView);
-        root.addRow(2,statusLabel, progressBar);
-        
-        this.getStyleClass().add("pane");
-        this.setCenter(root);
     }
-    
-    public void historyListener(Change<? extends Entry> changeValue) {
+
+    public void historyListener(ListChangeListener.Change<? extends WebHistory.Entry> changeValue) {
         changeValue.next();
-        for (Entry entry : changeValue.getRemoved()) {
+        for (WebHistory.Entry entry : changeValue.getRemoved()) {
             addressBox.getItems().remove(entry.getUrl());
             System.out.print("Removed url: ");
             System.out.println(entry.getUrl());
         }
-        for (Entry entry : changeValue.getAddedSubList()) {
+        for (WebHistory.Entry entry : changeValue.getAddedSubList()) {
             System.out.print("Added url: ");
             addressBox.getItems().add(entry.getUrl());
             System.out.println(entry.getUrl());
         }
     }
-    
+
     public void progressBarListener(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
         System.out.println("E");
         System.out.println(new_val.toString());
         progressBar.setProgress(new_val.doubleValue());
     }
-    
+
     private void stateChangeListener(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
         setWebpageTheme(false);
         String output = newValue.toString().toLowerCase();
         statusLabel.setText("Status: " + output);
     }
-    
+
     private void urlChangeListener(ObservableValue<? extends String> observable, String oldValue, String newValue) {
         addressBox.setValue(newValue);
     }
-    
+
     public void forwardButtonListener(ActionEvent event) {
         webEngine.executeScript("history.forward()");
     }
-    
+
     private void backButtonListener(ActionEvent event) {
         webEngine.executeScript("history.back()");
     }
-    
+
     private void searchButtonListener(ActionEvent event) {
         String google = "http://www.google.com/search?q=" + searchField.getText();
         webEngine.load(google.startsWith("http://") || google.startsWith("https://")
                 ? google : "http://" + google);
     }
-    
+
     private void proceedButtonListener(ActionEvent event) {
         String url = addressBox.valueProperty().getValue();
         webEngine.load(url.startsWith("http://") || url.startsWith("https://")
                 ? url : "http://" + url);
     }
-    
+
     private void setWebpageTheme(Boolean succeeded) {
         // Can safely access DOM and set styles.
         if (succeeded == true) {
@@ -177,7 +142,7 @@ public class WebBrowser extends BorderPane {
         }
         enableFirebug(false);
     }
-    
+
     /**
      * Enables debugging a webEngine.
      */
@@ -191,3 +156,4 @@ public class WebBrowser extends BorderPane {
         }
     }
 }
+
