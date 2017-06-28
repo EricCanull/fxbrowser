567a6931df01e1936d6f2a0b935d1d05f9ef8f6a
Eric Canull <ericcanull1000@msn.com>
2017-06-27 22:12:12 -0500

    add comments and rm unnessary css

--- src/main/java/WebController.java
+++ src/main/java/WebController.java
@@ -3,7 +3,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -19,11 +18,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- * Created by andje22 on 6/23/17.
+ * This is a basic web-browser demo using javafx.
+ *
+ * Created by Eric Canull on 6/23/17.
  */
 public class WebController extends BorderPane implements Initializable {
 
@@ -40,11 +39,12 @@ public class WebController extends BorderPane implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-
         initializeBrowser();
     }
 
+    /**
+     * Initialize the browser GUI and add event handlers
+     */
     private void initializeBrowser() {
         backButton.setText("\uD83E\uDC78");
         backButton.setOnAction(this::backButtonListener);
@@ -67,9 +67,8 @@ public class WebController extends BorderPane implements Initializable {
         searchButton.setDefaultButton(true);
         searchButton.setOnAction(this::searchButtonListener);
 
-
         webEngine = webView.getEngine();
-      //  webEngine.setUserStyleSheetLocation(getClass().getResource("/style/style.css").toString());
+        //webEngine.setUserStyleSheetLocation(getClass().getResource("/style/style.css").toString());
         webEngine.load(addressBox.getValue());
         progressBar.progressProperty().bind(webEngine.getLoadWorker().progressProperty());
         webEngine.getLoadWorker().stateProperty().addListener(this::stateChangeListener);
@@ -78,56 +77,98 @@ public class WebController extends BorderPane implements Initializable {
         history.getEntries().addListener(this::historyListener);
     }
 
+    /**
+     * Maintains web history for returning to a previous URL or vis-versa.
+     * @param changeValue A website page URL
+     */
     public void historyListener(ListChangeListener.Change<? extends WebHistory.Entry> changeValue) {
         changeValue.next();
+        // Removes url from
         for (WebHistory.Entry entry : changeValue.getRemoved()) {
             addressBox.getItems().remove(entry.getUrl());
-            System.out.print("Removed url: ");
-            System.out.println(entry.getUrl());
+            //System.out.print("Removed url: ");
+            //System.out.println(entry.getUrl());
         }
         for (WebHistory.Entry entry : changeValue.getAddedSubList()) {
-            System.out.print("Added url: ");
+            //System.out.print("Added url: ");
             addressBox.getItems().add(entry.getUrl());
-            System.out.println(entry.getUrl());
+            //System.out.println(entry.getUrl());
         }
     }
 
+    /**
+     * Updates the progress bar loading of an URL and resources.
+     * @param ov
+     * @param old_val
+     * @param new_val
+     */
     public void progressBarListener(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
-        System.out.println("E");
-        System.out.println(new_val.toString());
+        //System.out.println(new_val.toString());
         progressBar.setProgress(new_val.doubleValue());
     }
 
+    /**
+     * Updates the status label loading of an URL and resources.
+     * @param observable
+     * @param oldValue
+     * @param newValue
+     */
     private void stateChangeListener(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
-        setWebpageTheme(false);
+        setWebpageTheme(false); //set website html dark theme
         String output = newValue.toString().toLowerCase();
         statusLabel.setText("Status: " + output);
     }
 
+    /**
+     * Updates the address box URL when website is changed.
+     * @param observable
+     * @param oldValue
+     * @param newValue
+     */
     private void urlChangeListener(ObservableValue<? extends String> observable, String oldValue, String newValue) {
         addressBox.setValue(newValue);
     }
 
+    /**
+     * Use javaScript command to cycle forward in web history after forward button is pressed.
+     * @param event
+     */
     public void forwardButtonListener(ActionEvent event) {
         webEngine.executeScript("history.forward()");
     }
 
+    /**
+     * Use javaScript command to cycle backward in web history after forward button is pressed.
+     * @param event
+     */
     private void backButtonListener(ActionEvent event) {
         webEngine.executeScript("history.back()");
     }
 
+    /**
+     * Search the web using the contents of search field and google search.
+     * @param event
+     */
     private void searchButtonListener(ActionEvent event) {
         String google = "http://www.google.com/search?q=" + searchField.getText();
         webEngine.load(google.startsWith("http://") || google.startsWith("https://")
                 ? google : "http://" + google);
     }
 
+    /**
+     * Request URL in the address box after the proceed button has been pressed.
+     * @param event
+     */
     private void proceedButtonListener(ActionEvent event) {
         String url = addressBox.valueProperty().getValue();
         webEngine.load(url.startsWith("http://") || url.startsWith("https://")
                 ? url : "http://" + url);
     }
 
+    /**
+     * Sets the dark html theme on all websites.
+     * @param succeeded
+     */
     private void setWebpageTheme(Boolean succeeded) {
         // Can safely access DOM and set styles.
         if (succeeded == true) {
@@ -145,6 +186,7 @@ public class WebController extends BorderPane implements Initializable {
 
     /**
      * Enables debugging a webEngine.
+     * @param enabled
      */
     private void enableFirebug(Boolean enabled) {
         if (enabled) {
